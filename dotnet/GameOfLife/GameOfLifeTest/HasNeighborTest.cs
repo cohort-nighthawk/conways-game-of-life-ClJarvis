bde7bace1dfde43b8eed910f7deac714c0ba4027
Chris_Jarvis <clj@ollure.com>
2015-10-03 19:14:39 -0500

    repaired test project links

---
+++ dotnet/GameOfLife/GameOfLifeTest/HasNeighborTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTest
+{
+    [TestClass]
+    public class HasNeighbor
+    {
+        [TestMethod]
+        public void HasNeighbor()
+        {
+            bool neighbor = false;
+        }
+    }
+}
+
---
+++ dotnet/GameOfLife/GameOfLifeTest/NeighborTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTest
+{
+    [TestClass]
+    public class NeighborTest
+    {
+        public void HasNeighbor()
+        {
+            bool neighbor = false;
+        }
+    }
+}
+}
